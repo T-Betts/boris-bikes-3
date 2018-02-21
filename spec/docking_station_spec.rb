29e33105262364b1bda1dd9693edfee6bf9d25af
Tom Betts <tom.betts@live.co.uk>
2018-02-21 12:38:32 +0000

    Complete challenge 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,20 @@
-require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   # it 'responds to release_bike' do
   #  expect(subject).to respond_to :release_bike
   # end
   it { is_expected.to respond_to :release_bike}
+
+  # station = DockingStation.new
+  # it 'gets a bike and expects bike to be working' do
+  #   expect((station.release_bike).nil?).to eq false
+  #   expect((Bike.new).working?).to eq true
+  # end
+
+  it 'gets a bike and expects bike to be working' do
+    expect(subject.release_bike).to be_instance_of Bike
+    expect((subject.release_bike).working?).to eq true
+  end
+
 end
